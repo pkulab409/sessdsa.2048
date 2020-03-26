683a0209e69bc9d3470af50b1d4fd77c2bca6229
Vittorio-Veneto-www <61824869+Vittorio-Veneto-www@users.noreply.github.com>
2020-03-26 08:09:28 +0800

    新的人工智障

--- player.py
+++ player.py
@@ -1,13 +1,19 @@
-def play(board_ln, board_col ,numbers, ownership, player, mode):
-    import random
-    if mode == 0:
-        ln=random.randrange(board_ln)
-        col=random.randrange(board_col*2)
-        i=0
-        while ownership[ln][col]!=player or numbers[ln][col] and i < 200:
-            ln=random.randrange(board_ln)
-            col=random.randrange(board_col*2)
-            i+=1
-        return [ln,col]
-    else:
-        return random.randrange(4)
+class Player:
+    def __init__(self):
+        pass
+
+    def play(self, board_ln, board_col, numbers, ownership, self_place, player, mode):
+        if mode == 0:
+            if random.randrange(2):
+                ln = random.randrange(board_ln)
+                col = random.randrange(board_col * 2)
+                i = 0
+                while [ln, col] == self_place[player ^ 1] or ownership[ln][col] == player or numbers[ln][col] and i < 200:
+                    ln = random.randrange(board_ln)
+                    col = random.randrange(board_col * 2)
+                    i += 1
+                return [ln, col]
+            else:
+                return self_place[player]
+        else:
+            return random.randrange(4)
