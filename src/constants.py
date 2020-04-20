73dfeefa26285ba8992696f6c40ad58a992c8e7a
SophieARG <62552868+SophieARG@users.noreply.github.com>
2020-04-21 00:09:44 +0800

    提供了一个可供使用的棋盘对象，你想要的他都有（夸张）

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 ROWS = 4        # 行总数
 COLUMNS = 8     # 列总数
 ROUNDS = 100    # 总回合数
-MAXTIME = 2  # 总时间限制
+MAXTIME = 3     # 总时间限制
 MAXLEVEL = 14   # 总级别数
 
 ARRAY = list(range(ROUNDS))  # 随机(?)列表
@@ -9,8 +9,6 @@ ARRAY = list(range(ROUNDS))  # 随机(?)列表
 NAMES = {_: str(2 ** _).zfill(4) for _ in range(MAXLEVEL)}  # 将内在级别转换为显示对象的字典
 NAMES[0] = '0000'
 
-INIT_BELONG = [[0 if _ < COLUMNS // 2 else 1 for _ in range(COLUMNS)] for _ in range(ROWS)]  # 初始化领域
-INIT_PLATFORM = [[0 for _ in range(COLUMNS)] for _ in range(ROWS)]  # 初始化棋盘
 DIRECTIONS = {0: 'up', 1: 'down', 2: 'left', 3: 'right', None: 'None'}    # 换算方向的字典
 
 PLAYERS = ['nanami', 'ayase']   # 游戏图片名称
@@ -27,3 +25,131 @@ COLOR_WORD = {'+': '#776e65', '-': '#f9f6f2'}  # 双方的文字色
 
 KEY_BACKWARD = "\'[\'"  # 回退
 KEY_FORWARD = "\']\'"   # 前进
+
+# 棋子
+
+class Chessman:
+    def __init__(self, belong, position, value = 1):
+        '''
+        -> 初始化棋子
+        -> 参数: belong   归属, 为bool, True代表先手
+        -> 参数: position 位置, 为tuple
+        -> 参数: value    数值, 为int
+        '''
+        self.belong = belong
+        self.position = position
+        self.value = value
+
+# 棋盘
+
+class Chessboard:
+    def __init__(self, array):
+        '''
+        -> 初始化棋盘
+        '''
+        self.array = array  # 随机序列
+        self.board = {}  # 棋盘所有棋子
+        self.belongs = {True:[], False:[]}  # 双方的棋子位置
+
+    def add(self, belong, position, value = 1):
+        '''
+        -> 在指定位置下棋
+        '''
+        self.belongs[belong].append(position)
+        self.board[position] = Chessman(belong, position, value)
+
+    def move(self, belong, direction):
+        '''
+        -> 向指定方向合并, 返回是否变化
+        '''
+        def inBoard(position):  # 判断是否在棋盘内
+            return position[0] in range(ROWS) and position[1] in range(COLUMNS)
+        def isMine(position):   # 判断是否在领域中
+            return belong if position[1] < COLUMNS // 2 else not belong
+        def theNext(position):  # 返回下一个位置
+            delta = [(-1,0), (1,0), (0,-1), (0,1)][direction]
+            return (position[0] + delta[0], position[1] + delta[1])
+        def conditionalSorted(chessmanList):  # 返回根据不同的条件排序结果
+            if direction == 0: return sorted(chessmanList, key = lambda x:x[0], reverse = False)
+            if direction == 1: return sorted(chessmanList, key = lambda x:x[0], reverse = True )
+            if direction == 2: return sorted(chessmanList, key = lambda x:x[1], reverse = False)
+            if direction == 3: return sorted(chessmanList, key = lambda x:x[1], reverse = True )
+        def move_one(chessman, eaten):  # 移动一个棋子并返回是否移动, eaten是已经被吃过的棋子位置
+            nowPosition = chessman.position
+            nextPosition = theNext(nowPosition)
+            while inBoard(nextPosition) and isMine(nextPosition) and nextPosition not in self.board:  # 跳过己方空格
+                nowPosition = nextPosition
+                nextPosition = theNext(nextPosition)
+            if inBoard(nextPosition) and nextPosition in self.board and nextPosition not in eaten \
+                    and chessman.value == self.board[nextPosition].value:  # 满足吃棋条件
+                self.belongs[belong].remove(chessman.position)
+                self.belongs[belong if nextPosition in self.belongs[belong] else not belong].remove(nextPosition)
+                self.belongs[belong].append(nextPosition)
+                self.board[nextPosition] = Chessman(belong, nextPosition, chessman.value + 1)
+                del self.board[chessman.position]
+                eaten.append(nextPosition)
+                return True
+            elif nowPosition != chessman.position:  # 不吃棋但移动了
+                self.belongs[belong].remove(chessman.position)
+                self.belongs[belong].append(nowPosition)
+                self.board[nowPosition] = Chessman(belong, nowPosition, chessman.value)
+                del self.board[chessman.position]
+                return True
+            else:  # 未发生移动
+                return False
+        eaten = []
+        change = False
+        for _ in conditionalSorted(self.belongs[belong]):
+            if move_one(self.board[_], eaten): change = True
+        return change
+
+    def getBelong(self, position):
+        '''
+        -> 返回归属
+        '''
+        return self.board[position].belong if position in self.board else position[1] < COLUMNS // 2
+
+    def getValue(self, position):
+        '''
+        -> 返回数值
+        '''
+        return self.board[position].value if position in self.board else 0
+
+    def getScore(self, belong):
+        '''
+        -> 返回某方的全部棋子数值列表
+        '''
+        return list(map(lambda x: self.board[x].value, self.belongs[belong]))
+
+    def getNone(self, belong):
+        '''
+        -> 返回某方的全部空位列表
+        '''
+        return [(row, column) for row in range(ROWS) for column in range(COLUMNS) \
+                if ((column < COLUMNS // 2) == belong) and (row, column) not in self.board]
+    
+    def getNext(self, belong, currentRound):
+        '''
+        -> 根据随机序列得到在本方领域允许下棋的位置
+        '''
+        available = self.getNone(belong)
+        return available[self.array[currentRound] % len(available)] if available != [] else None
+
+    def copy(self):
+        '''
+        -> 返回一个对象拷贝
+        '''
+        new = Chessboard(self.array)
+        new.board = self.board.copy()
+        new.belongs[True] = self.belongs[True].copy()
+        new.belongs[False] = self.belongs[False].copy()
+        return new
+
+    def __repr__(self):
+        '''
+        -> 打印棋盘, + 代表先手, - 代表后手
+        '''       
+        return '\n'.join([' '.join([('+' if self.getBelong((row, column)) else '-') + str(self.getValue((row, column))).zfill(2) \
+                                   for column in range(COLUMNS)]) \
+                         for row in range(ROWS)])
+    __str__ = __repr__
