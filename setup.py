2da3a4a3eaa5d448656a33a60ebf592c4f5a3d41
HamiltonHuaji <zhangyuhao415@gmail.com>
2020-05-07 13:14:30 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 import pybind11
 setup(
     name="libchessboard",
     version="0.0.1",
-    packages=find_packages(),
     setup_requires=["pybind11"],
     ext_modules=[
         Extension("libchessboard", ["chessboard.cpp"],
                   include_dirs=[pybind11.get_include()], language='c++',
-                  extra_compile_args=["-std=c++17"])
+                  extra_compile_args=["-std=c++17"],
+                  extra_link_args=["-static-libgcc", "-static-libstdc++"])
     ],
     author="HamiltonHuaji",
     description="Yet Another libchessboard",
