3b5ccf9cf3147a3ae1282390f5082da47cdf99cd
SophieARG <62552868+SophieARG@users.noreply.github.com>
2020-05-07 17:27:50 +0800

    后手序列翻转

--- src/constants.py
+++ src/constants.py
@@ -137,15 +137,14 @@ class Chessboard:
         -> 返回某方的全部空位列表
         '''
         return [(row, column) for row in range(ROWS) for column in range(COLUMNS) \
-                if column < COLUMNS // 2 and (row, column) not in self.board] if belong \
-          else [(row, column) for row in reversed(range(ROWS)) for column in reversed(range(COLUMNS)) \
-                if column >= COLUMNS // 2 and (row, column) not in self.board]
+                if ((column < COLUMNS // 2) == belong) and (row, column) not in self.board]
     
     def getNext(self, belong, currentRound):
         '''
         -> 根据随机序列得到在本方领域允许下棋的位置
         '''
         available = self.getNone(belong)
+        if not belong: available.reverse()  # 后手序列翻转
         return available[self.array[currentRound] % len(available)] if available != [] else None
 
     def getAnime(self):
