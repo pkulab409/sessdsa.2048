0e4153cb74fe188a49954a9b8138aa2a15815c3a
doctorwho-debug <62594979+doctorwho-debug@users.noreply.github.com>
2020-05-29 18:36:16 +0800

    Add files via upload
    使用方法：
    一、进行比赛的交互命令有：
    F19 first  进行F19级第一轮比赛
    F19 8 4   进行F19级8进4比赛
    F19 4 2   进行F19级4进2比赛
    F19 final  进行F19级冠军争夺比赛
    
    N19 8 4  进行N19级8进4比赛
    N19 4 2 进行N19级4进2比赛
    N19 third 进行N19级季军争夺比赛
    N19 final 进行N19级冠军争夺比赛
    
    all final 进行友谊赛
    
    Yes 每两次比赛间汇报出线/夺冠小组的详细信息
    No 每两次比赛间汇报跳过汇报出线/夺冠小组的详细信息
    
    Break  每次比赛输入开始命令(以F19，N19开头的命令）前，可以输入Break终止整个比赛，重置开始
    
    Yes 全部比赛结束后，汇报比赛所有的冠亚季军
    No 直接结束比赛
    
    二、使用环境和方法
    1.将F19级天梯赛出线的12个小组代码，按名次，依次用playerx.py命名（x为该小组的名次）
    2..将N19级天梯赛出线的8个小组代码，按名次，依次用Playery.py命名（y为该小组的名次,P大写）
    3.将每个小组想展示的额外信息写入information包的字典对应的位置中，如小组成员的名字等
    4.将singlematchrunner.py、information.py、F19players代码包、Fmultimatchrunner.py,Nmultimatchrunner.py,friendship.py,N19players代码包和其他全部的的比赛设施一起全部放入同一个src文件夹中
    5.用两个解释器同时分别运行程序Fmultimatchrunner.py，Nmultimatchrunner.py，运行过程中分别依次根据输入提示，和约定的字符串，控制每一大组的比赛进行。
    6.先完成步骤5后，再运行friendship.py，根据交互命令，进行友谊赛
    7.注：每个比赛的主函数含唯一参数livequeue，默认为None，可以根据直播需要引出调用。
    三、输出文件
    1.每场比赛都会输出一个以该场比赛名字命名的文件夹
    2.每个文件夹内会有记录这场比赛所有的对战记录的子文件夹（包括每一轮比赛的复盘和总的统计报告），每个子文件夹以“xxx vs yyy"命名
    3.每一场比赛的文件夹内会有一个记录该场比赛出线队伍名单的txt文档，命名为winners.txt
    4.每一大组比赛完成后会输出两个记录冠亚季军的文档N19results.txt,F19results.txt

---
+++ MatchRunner(version1.3.0)/friendship.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 13:50:50 2020
+
+@author: 田晨霄
+"""
+import sys
+import os
+
+import singlematchrunner as runner
+import constants as c
+import information as inf
+
+
+def read_txt(input_dir):#
+    im_names=[]
+    ms=open(input_dir)
+    for eachline in ms:
+        eachline=eachline.strip('\n')
+        eachline=str(eachline)
+        line=eachline
+        im_names.append(line)
+    return im_names
+s1=read_txt("F19results.txt")
+s2=read_txt("N19results.txt")
+NwinnerN19=s2[0]
+secondN19=s2[1]
+thirdN19=s2[2]
+thirdsF191=s1[2]
+thirdsF192=s1[3]
+FwinnerF19=s1[0]
+secondF19=s1[1]
+def friendship(livequeue=None):
+    def mkdir(path):
+        folder=os.path.exists(path)
+        if folder==False:
+            os.makedirs(path)
+        else:
+            path=path+"_"+"1"
+            while os.path.exists(path)==True:
+                path=path[0:-1]+str(int(path[-1])+1)
+            os.makedirs(path)
+        return path
+    
+    """=========================================================================="""
+    ####Friendship####
+    s=input("Please input all final to start match all final : ")
+    while s!= "all final":
+        if s=="Break":
+            sys.exit(0)
+        s=input("Please input all final to start match all final : ")
+    finalplayerlist=[NwinnerN19,FwinnerF19]
+    path=mkdir('all final')
+    winnerfinal=runner.main(finalplayerlist,path+"/"+" "+NwinnerN19[11:-3]+"vs"+" "+FwinnerF19[11:-3],livequeue,True,True,True,False,c.REPEAT,c.MAXTIME,c.ROUNDS)[3]
+    print(winnerfinal)
+    f = open(path+'/'+"all final.txt", 'w')
+    f.write("all final winners:"+"\n")
+    f.write(winnerfinal)
+    f.close()
+    while s!="No":
+        if s=="Yes":
+            print("恭喜以下小组同学获得本次对抗2048比赛总冠军!")
+            print(inf.information[winnerfinal[11:-3]])
+            break
+        else:
+            s=input("是否显示总冠军小组详细信息 (Yes or No,输入No进入颁奖) : ")
+    """=============================================================================="""
+    ###比赛冠亚季军汇报###
+    s=input("是否输入命令汇报比赛冠亚季军结果 (Yes or No) : ")
+    while s!="No":
+        if s=="Yes":
+            print("恭喜以下小组同学获得本次对抗2048比赛冠亚季军!")
+            print("F19组季军: ")
+            print(thirdsF191)
+            print(thirdsF192)
+            print("F19组亚军: ")
+            print(secondF19)
+            print("F19组冠军: ")
+            print(inf.information[FwinnerF19[11:-3]])
+            print("N19组季军: ")
+            print(thirdN19)
+            print("N19组亚军: ")
+            print(secondN19)
+            print("N19组冠军: ")
+            print(inf.information[NwinnerN19[11:-3]])
+            print("总冠军: ")
+            print(inf.information[winnerfinal[11:-3]])
+            break
+        else:
+            s=input("是否输入 Report 命令开始汇报比赛冠亚季军结果 (Yes or No,输入No结束比赛) : ")
+    print("比赛结束!")
+
+friendship(None)
