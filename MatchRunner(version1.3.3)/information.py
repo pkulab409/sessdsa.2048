534f3b8f0611a5412b2027b955a6747b78f8d2a1
doctorwho-debug <62594979+doctorwho-debug@users.noreply.github.com>
2020-06-02 01:17:18 +0800

    Update information.py

--- MatchRunner(version1.3.3)/information.py
+++ MatchRunner(version1.3.3)/information.py
@@ -4,8 +4,8 @@ Created on Fri May 29 00:31:46 2020
 
 @author: 田晨霄
 """
-F19=["Uniform ","Echo ","Zulu ","Golf ","November ","Sierra ","X-ray ","3721 ","Bravo ","007 ","666 ","Remeo "]
-N19=[" Sierra"," X-ray"," 777"," Bravo"," 666"," 404"," 8848"," Remeo"]
+F19=["Uniform","Echo","Zulu","Golf","November","Sierra ","X-ray","3721","Bravo","007","666","Remeo"]
+N19=["Sierra"," X-ray","777"," Bravo","666","404","8848"," Remeo"]
 information={}
 for i in range(len(F19)):
     information["player"+str(i+1)]=F19[i]
